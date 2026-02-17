feefa15c517a610723b66eaa4117f18c358206d2
Sparsh Aggarwal <sparshagg1606@gmail.com>
2026-02-17 18:42:07 +0530

    final deploy changes

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://style-gen-plum.vercel.app/",
+    origin: "https://style-gen-frontend.vercel.app",
     credentials: true,
   }),
 );
